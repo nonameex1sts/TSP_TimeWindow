e864b49e92248920a37ae5184547e741c0a49d69
hieu <mhieu.vn@gmail.com>
2023-10-15 19:29:17 +0700

    New CP

---
+++ CP/CP_TSPTimeWindow_FileInput.py
@@ -0,0 +1,123 @@
+from ortools.sat.python import cp_model
+import time
+
+# Bien: X: x[i][j] = 1 neu di tu thanh pho i den thanh pho j, 0 neu nguoc lai
+#       M: M[i] = thoi gian den thanh pho i
+#       w: w[i] = thoi gian cho tai thanh pho i
+# Rang buoc:
+#       - Moi thanh pho chi duoc di qua 1 lan
+#       - Thoi gian den thanh pho i phai nam trong khoang [e[i], l[i]]
+#       - Thoi gian den thanh pho j = tg den thanh pho i + tg giao hang tai i + tg di tu i den j + tg cho tai j
+# Muc tieu: Minimize tong thoi gian di chuyen giua cac thanh pho
+# -----------------------------------------------
+
+inputFolder = "testcase/input"
+outputFolder = "testcase/output"
+timeFile = "CP/time.txt"
+allFile = ["N5.txt", "N10.txt", "N100.txt", "N200.txt", "N300.txt", "N500.txt", "N600.txt", "N700.txt", "N900.txt",
+           "N1000.txt"]
+
+
+def CP_TSP_TimeWindow(file, num_nodes, e, l, d, time_matrix):
+    x = {}  # x[i,j] = 1 if i -> j else 0
+    M = {}  # M[i] = thoi gian giao hang tai thanh pho i
+    w = {}  # w[i] = thoi gian cho tai thanh pho i
+
+    # Model
+    model = cp_model.CpModel()
+
+    for i in range(num_nodes + 1):
+        for j in range(num_nodes + 1):
+            x[i, j] = model.NewIntVar(0, 1, 'x[%i, %i]' % (i, j))
+
+    # Thoi gian den thanh pho i phai nam trong khoang [e[i], l[i]]
+    for i in range(num_nodes + 1):
+        M[i] = model.NewIntVar(e[i], l[i], 'M[%i]' % i)
+        w[i] = model.NewIntVar(0, 1000000, 'w[%i]' % i)
+
+    # Khoi tao gia tri cho M[0] va w[0]
+    model.Add(M[0] == 0)
+    model.Add(w[0] == 0)
+
+    # Moi thanh pho chi duoc di qua 1 lan
+    for i in range(num_nodes + 1):
+        model.Add(sum(x[i, j] for j in range(num_nodes + 1) if j != i) == 1)
+        model.Add(sum(x[j, i] for j in range(num_nodes + 1) if j != i) == 1)
+
+    # Thoi gian den thanh pho j = tg den thanh pho i + tg giao hang tai i + tg di tu i den j + tg cho tai j
+    for i in range(num_nodes + 1):
+        for j in range(num_nodes + 1):
+            if i != j and j != 0:
+                model.Add(M[j] >= M[i] + d[i] + time_matrix[i, j] + w[j] + (x[i, j] - 1) * 1000000000)
+                model.Add(M[j] <= M[i] + d[i] + time_matrix[i, j] + w[j] + (1 - x[i, j]) * 1000000000)
+
+    # Muc tieu: Minimize tong thoi gian di het cac thanh pho
+    model.Minimize(M[num_nodes] + d[num_nodes] + time_matrix[num_nodes, 0])
+
+    # Solve model
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    # Result
+    if status == cp_model.OPTIMAL:
+        return solver.ObjectiveValue()
+    else:
+        print("No solution found.")
+        return 0
+
+
+if __name__ == '__main__':
+    for file in allFile:
+        print(file)
+
+        num_nodes = 0
+        e = {} # thoi gian giao hang som nhat tai thanh pho i
+        l = {} # thoi gian giao hang tre nhat tai thanh pho i
+        d = {} # thoi gian mat de giao hang tai thanh pho i
+        time_matrix = {} # time_matrix[i][j] = thoi gian di tu thanh pho i den thanh pho j
+
+        # Read input
+        with open(f"{inputFolder}/{file}", "r") as f:
+            num_nodes = int(f.readline())
+
+            e[0] = 0
+            l[0] = 1000000000
+            d[0] = 0
+
+            for i in range(1, num_nodes + 1):
+                data = f.readline().split(" ")
+
+                e[i] = int(data[0])
+                l[i] = int(data[1])
+                d[i] = int(data[2])
+
+            for i in range(num_nodes + 1):
+                data = f.readline().split(" ")
+                for j in range(num_nodes + 1):
+                    time_matrix[i, j] = int(data[j])
+
+        # Run TSP for each file
+        startTime = time.time()
+        result = CP_TSP_TimeWindow(file, num_nodes, e, l, d, time_matrix)
+        endTime = time.time()
+
+        # Write time to file
+        with open(timeFile, "a") as fTime:
+            fTime.write(f"{file}: {endTime - startTime}\n")
+
+        # Compare result
+        with open(f"{outputFolder}/{file}", "r") as fOutput:
+            num_nodes = int(fOutput.readline())
+            list_cities = list(map(int, fOutput.readline().split()))
+
+            timeOutput = time_matrix[0, list_cities[0]]
+            for i in range(num_nodes):
+                if i < num_nodes - 1:
+                    timeOutput += time_matrix[list_cities[i], list_cities[i + 1]]
+                else:
+                    timeOutput += time_matrix[list_cities[i], 0]
+
+            if result == timeOutput:
+                print(f"{file} is correct")
+            else:
+                print(f"{file} is wrong")
---
+++ CP/CP_TSPTimeWindow_KeyboardInput.py
@@ -0,0 +1,95 @@
+from ortools.sat.python import cp_model
+
+# Bien: X: x[i][j] = 1 neu di tu thanh pho i den thanh pho j, 0 neu nguoc lai
+#       M: M[i] = thoi gian den thanh pho i
+#       w: w[i] = thoi gian cho tai thanh pho i
+# Rang buoc:
+#       - Moi thanh pho chi duoc di qua 1 lan
+#       - Thoi gian den thanh pho i phai nam trong khoang [e[i], l[i]]
+#       - Thoi gian den thanh pho i = tg den thanh pho j + tg giao hang tai j + tg di tu j den i + tg cho tai j
+# Muc tieu: Minimize tong thoi gian di chuyen giua cac thanh pho
+# -----------------------------------------------
+
+
+e = {}  # thoi gian som nhat de den thanh pho i
+l = {}  # thoi gian tre nhat de den thanh pho i
+d = {}  # thoi gian mat de giao hang tai thanh pho i
+x = {}  # x[i,j] = 1 if i -> j else 0
+M = {}  # M[i] = thoi gian giao hang tai thanh pho i
+w = {}  # w[i] = thoi gian cho tai thanh pho i
+time_matrix = {}  # time_matrix[i][j] = thoi gian di tu thanh pho i den thanh pho j
+
+# Input
+num_nodes = int(input())
+
+e[0] = 0
+l[0] = 1000000000
+d[0] = 0
+
+for i in range(1, num_nodes + 1):
+    data = input().split(" ")
+
+    e[i] = int(data[0])
+    l[i] = int(data[1])
+    d[i] = int(data[2])
+
+for i in range(num_nodes + 1):
+    data = input().split(" ")
+    for j in range(num_nodes + 1):
+        time_matrix[i, j] = int(data[j])
+
+# Model
+model = cp_model.CpModel()
+
+for i in range(num_nodes + 1):
+    for j in range(num_nodes + 1):
+        x[i, j] = model.NewIntVar(0, 1, 'x[%i, %i]' % (i, j))
+
+# Thoi gian den thanh pho i phai nam trong khoang [e[i], l[i]]
+for i in range(num_nodes + 1):
+    M[i] = model.NewIntVar(e[i], l[i], 'M[%i]' % i)
+    w[i] = model.NewIntVar(0, 1000000, 'w[%i]' % i)
+
+# Khoi tao gia tri cho M[0] va w[0]
+model.Add(M[0] == 0)
+model.Add(w[0] == 0)
+
+# Moi thanh pho chi duoc di qua 1 lan
+for i in range(num_nodes + 1):
+    model.Add(sum(x[i, j] for j in range(num_nodes + 1) if j != i) == 1)
+    model.Add(sum(x[j, i] for j in range(num_nodes + 1) if j != i) == 1)
+
+# Thoi gian den thanh pho i = thoi gian den thanh pho j + thoi gian di tu j den i
+for i in range(num_nodes + 1):
+    for j in range(num_nodes + 1):
+        if i != j and j != 0:
+            model.Add(M[j] >= M[i] + d[i] + time_matrix[i, j] + w[j] + (x[i, j] - 1) * 1000000000)
+            model.Add(M[j] <= M[i] + d[i] + time_matrix[i, j] + w[j] + (1 - x[i, j]) * 1000000000)
+
+# Muc tieu: Minimize tong thoi gian di het cac thanh pho
+model.Minimize(M[num_nodes] + d[num_nodes] + time_matrix[num_nodes, 0])
+
+# Solve model
+solver = cp_model.CpSolver()
+status = solver.Solve(model)
+
+# In ket qua
+print(num_nodes)
+
+if status == cp_model.OPTIMAL:
+    # In gia tri toi uu
+    # print(solver.ObjectiveValue())
+
+    # In hanh trinh toi uu
+    current_city = 0
+    while 1:
+        for i in range(num_nodes + 1):
+            if solver.Value(x[current_city, i]) == 1:
+                if i != 0:
+                    print(i, end=' ')
+                current_city = i
+                break
+        if current_city == 0:
+            break
+else:
+    print("No solution found.")
